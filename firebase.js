2e8f59fde6bd2a0d81963c6ae5de02a4daaa9f27
Karthikkid <106878427+Karthikkid@users.noreply.github.com>
2022-06-04 20:45:49 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC09w2Dz3BFhhqKXIIbj8BpJnVgQYsXiFE",
+  authDomain: "dbms-597de.firebaseapp.com",
+  projectId: "dbms-597de",
+  storageBucket: "dbms-597de.appspot.com",
+  messagingSenderId: "401606904851",
+  appId: "1:401606904851:web:7bbddd0d28a678230a03e6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
